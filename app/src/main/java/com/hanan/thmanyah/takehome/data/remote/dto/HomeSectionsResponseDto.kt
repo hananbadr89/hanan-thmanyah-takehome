dd0325bc53011e9f2f33c00a4d4170a132cd5d23
Hanan Alqahtani <hanan.algahtani89@gmail.com>
2026-01-09 14:14:20 +0300

    feat: Update HomeSectionsResponseDto

--- app/src/main/java/com/hanan/thmanyah/takehome/data/remote/dto/HomeSectionsResponseDto.kt
+++ app/src/main/java/com/hanan/thmanyah/takehome/data/remote/dto/HomeSectionsResponseDto.kt
@@ -5,24 +5,29 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class HomeSectionsResponseDto(
-    val sections: List<SectionDto>,
-    val pagination: PaginationDto
+    val sections: List<HomeSectionDto>? = null,
 )
 
 @JsonClass(generateAdapter = true)
-data class PaginationDto(
-    @property:Json(name = "next_page")
-    val nextPage: String?,
-    @property:Json(name = "total_pages")
-    val totalPages: Int?
-)
-
-@JsonClass(generateAdapter = true)
-data class SectionDto(
-    val name: String?,
-    val type: String?,
+data class HomeSectionDto(
+    val name: String? = null,
+    val type: String? = null,
     @property:Json(name = "content_type")
-    val contentType: String?,
-    val order: Int?,
-    val content: List<Map<String, Any?>>
+    val contentType: String? = null,
+    val order: Int? = null,
+    val content: List<HomeContentDto>? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class HomeContentDto(
+    @property:Json(name = "podcast_id") val podcastId: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    @property:Json(name = "avatar_url") val avatarUrl: String? = null,
+    @property:Json(name = "episode_count") val episodeCount: Int? = null,
+    val duration: Int? = null,
+    val language: String? = null,
+    val priority: Int? = null,
+    val popularityScore: Int? = null,
+    val score: Double? = null
 )
