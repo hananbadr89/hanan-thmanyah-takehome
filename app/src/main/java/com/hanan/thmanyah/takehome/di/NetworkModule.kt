79c04e2d48282715cc5ee131305c709f13e710f0
Hanan Alqahtani <hanan.algahtani89@gmail.com>
2026-01-09 14:13:28 +0300

    feat: Update home api and add network module

---
+++ app/src/main/java/com/hanan/thmanyah/takehome/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.hanan.thmanyah.takehome.di
+
+import com.hanan.thmanyah.takehome.data.remote.ThmanyahHomeApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api-v2-b2sit6oh3a-uc.a.run.app/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                }
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        moshi: Moshi,
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideThmanyahApi(
+        retrofit: Retrofit
+    ): ThmanyahHomeApi =
+        retrofit.create(ThmanyahHomeApi::class.java)
+}
