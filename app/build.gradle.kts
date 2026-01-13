5f2bde01f851e7d1e77b6e4da1bd92d1649e6db0
Hanan Alqahtani <hanan.algahtani89@gmail.com>
2026-01-13 11:49:08 +0300

    build(test): add unit test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,7 @@ dependencies {
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     kapt(libs.room.compiler)
+
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutines.test)
 }
